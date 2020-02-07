3b4572cc29964513655f914bb8921e661fd2b12b
Dare Balogun <mroludarebalogun@yahoo.ca>
2020-02-07 16:19:29 -0500

    Configure memory

---
+++ src/kernel/atag.c
@@ -0,0 +1,18 @@
+#include <kernel/atag.h>
+
+// Return the size of memory available, using atag info from bootloader
+uint32_t get_mem_size(atag_t *tag)
+{
+    // Qemu does not emulate bootloader but we can set qemu mem to 1024MB
+    return 1024 * 1024 * 1024;
+    while (tag->tag != NONE)
+    {
+        if (tag->tag == MEM)
+        {
+            // To test on actual hardware
+            return tag->mem.size;
+        }
+        tag = ((uint32_t *)tag) + tag->tag_size;
+    }
+    return 0;
+}
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,6 +1,13 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <kernel/uart.h>
+#include <kernel/atag.h>
+#include <kernel/mem.h>
+#include <kernel/kerio.h>
+#include <kernel/gpu.h>
+#include <common/stdlib.h>
+
+void int_to_string(uint32_t number, char *str);
 
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
@@ -14,11 +21,44 @@ extern "C" /* Use C linkage for kernel_main. */
     (void)atags;
 
     uart_init();
-    uart_puts("Hello, kernel World!\r\n");
+    puts("Welcome to PiKernel!\r\n");
+    puts("Initilizing Memory\n");
+
+    uint32_t mem_size = get_mem_size((atag_t *)atags);
+    char mem_size_str[1024];
+    int_to_string(mem_size, mem_size_str);
+    uart_puts(mem_size_str);
 
     while (1)
     {
         uart_putc(uart_getc());
         uart_putc('\n');
+    }
+}
+
+void int_to_string(uint32_t number, char *str)
+{
+    uint32_t n = number;
+    int i = 0;
+
+    while (n != 0)
+    {
+        str[i++] = n % 10 + '0';
+        n = n / 10;
+    }
+
+    str[i] = '\0';
+
+    for (int t = 0; t < i / 2; t++)
+    {
+        str[t] ^= str[i - t - 1];
+        str[i - t - 1] ^= str[t];
+        str[t] ^= str[i - t - 1];
+    }
+
+    if (number == 0)
+    {
+        str[0] = '0';
+        str[1] = '\0';
     }
 }
